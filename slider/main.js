f9d270de3b88a851318d65d059de8f9f051998e1
joatro <joatro@users.noreply.github.com>
2023-10-23 17:50:07 -0700

    Add files via upload

---
+++ slider/main.js
@@ -0,0 +1,50 @@
+class MyHeader extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML =`
+        
+        <header>
+        <a href= index.html>
+        
+        </a>
+        <nav>
+        <ul>
+        
+        <li><a href=index.html>Home</a>
+        <li><a href=about.html>About</a>
+        <li><a href=servicios.html>Servicios</a>
+        <li><a href=contact.html>Contact</a>
+
+        </ul>
+        
+        </nav>
+        </header>
+        
+        
+        
+        
+        `
+    }
+}
+
+customElements.define('my-header', MyHeader)
+
+
+class MyFooter extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML =`
+        
+        <footer>
+
+        @2023
+        My Name
+      
+        </footer>
+        
+        
+        
+        
+        `
+    }
+}
+
+customElements.define('my-footer', MyFooter)
